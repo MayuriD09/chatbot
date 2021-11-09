edf2cbcc160dd99f006ccd548636c2a42063bab4
Mayuri <67644114+MayuriD09@users.noreply.github.com>
2021-11-10 01:28:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 
 from main import chatWithBot
 
+import os
+
 app = Flask(__name__)
 
 
@@ -12,4 +14,6 @@ def chatBot():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   port = int(os.environ.get('PORT', 5000))
+	#run the app locally on the givn port
+	app.run(host='0.0.0.0', port=port)
